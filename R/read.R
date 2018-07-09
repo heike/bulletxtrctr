cc0b299eba6a2f454b6fbe220f6105977926ef99
Heike Hofmann <hofmann@iastate.edu>
2018-07-09 14:43:49 -0500

    reading set of scans

--- R/read.R
+++ R/read.R
@@ -4,11 +4,16 @@
 #' @param folder character describing the path to a folder
 #' @param ext character value, consisting of the extension(s) describing the file format the scans are in
 #' @export
+#' @importFrom x3ptools read_x3p
 #' @examples
-#'  b1 <- read_bullet("/Volumes/Ocean/bullet-scans/LAPD/FAU 609/Bullet A", "x3p")
-#'  b2 <- read_bullet("/Volumes/Ocean/bullet-scans/LAPD/FAU 609/Bullet B", "x3p")
+#'  b1 <- read_bullet("data/Bullet1", "x3p")
+#'  b2 <- read_bullet("data/Bullet2", "x3p")
 read_bullet <- function(folder, ext="x3p") {
   set = dir(folder, pattern=ext, recursive = TRUE, full.names = TRUE)
-  basenames <- basenames(set)
-  message(paste("The following files were found: ", basenames, collapse="\n"))
+  message(sprintf("%d files found. Reading ...", length(set)))
+  if (ext == "x3p") {
+    scans <- lapply(set, FUN = read_x3p)
+  }
+  names(scans) <- set
+  scans
 }
