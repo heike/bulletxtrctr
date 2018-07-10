23d9d31fbb510d26aefd58ef66f3aa7b5a0ef7a2
Susan Vanderplas <srvanderplas@gmail.com>
2018-07-10 13:29:16 -0500

    test read_bullet

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,7 +1,13 @@
 context("read")
 
+library(bulletxtrctr)
+library(tidyverse)
 
 test_that("read works as expected", {
-
-
+  o1 <- capture.output(b1 <- read_bullet("../testdata/Bullet1", "x3p"), split = T)
+  expect_equivalent(nrow(b1), 5)
+  expect_s3_class(b1, "tbl_df")
+  expect_s3_class(b1, "tbl")
+  expect_s3_class(b1, "data.frame")
+  expect_length(o1, 0)
 })
