a1f14e10338ccc9d9de86b0392e70c003795e09c
Heike Hofmann <hofmann@iastate.edu>
2018-07-12 11:52:28 -0500

    delete unused function

--- R/signatures.R
+++ R/signatures.R
@@ -41,26 +41,3 @@ cc_get_signature <- function(ccdata, grooves = NULL, span1 = 0.75, span2 = 0.03)
     ccdata
 }
 
-#' Smooth the raw signature to remove excess variability
-#'
-#' @param data data frame as returned by the function \code{cc_get_raw_signature}
-#' @param span width of the smoother, defaults to 0.03
-#' @param limits vector of the form c(min, max). Results will be limited to be between these values.
-#' @param id variable name of the land identifier
-#' @return data frame of the same form as the input extended by the vector sig for the smoothed signature.
-#' @importFrom dplyr mutate
-#' @export
-cc_get_signature_notused <- function(data, span = 0.03, limits = c(-5,5), id="bullet") {
-  bullet <- NULL
-  y <- NULL
-  myspan <- NULL
-
-  lof <- data %>% group_by_(id) %>% mutate(
-    myspan = ifelse(span > 1, span / diff(range(y)), span),
-    l30 = smoothloess(y, resid, span = myspan[1])
-  ) %>% select(-myspan)
-  lof$l30 <- pmin(max(limits), lof$l30)
-  lof$l30 <- pmax(min(limits), lof$l30)
-  lof
-}
-
