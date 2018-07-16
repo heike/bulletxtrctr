2d3e23c290d3f20b799206d545e84b573f94e5df
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-07-16 12:23:53 -0500

    small test file and code to generate it - uses data from Hamby 252

--- tests/testthat/test_crosscut.R
+++ tests/testthat/test_crosscut.R
@@ -4,10 +4,30 @@ library(x3ptools)
 library(bulletxtrctr)
 library(tidyverse)
 
-b1 <- read_bullet("../testdata/Bullet1", "x3p")
-b1_l1 <- x3ptools::x3p_to_df(b1$x3p[[1]])
+# b1 <- read_bullet("data/Bullet1", "x3p")
+# # turn the scans such that (0,0) is bottom left
+# b1 <- b1 %>% mutate(
+#   x3p = x3p %>% purrr::map(.f = function(x) x %>%
+#                              rotate_x3p(angle=-90) %>%
+#                              y_flip_x3p())
+# ) %>% mutate(
+#   x3p = x3p %>% purrr::map(.f = function(x) {
+#     # make sure all measurements are in microns
+#     x$surface.matrix <- x$surface.matrix*10^6
+#     x$header.info$incrementY <- x$header.info$incrementY*10^6
+#     x$header.info$incrementX <- x$header.info$incrementX*10^6
+#     x
+#   })
+# ) %>%
+#   filter(row_number() == 1)
+#
+#
+# b1_l1 <- x3ptools::x3p_to_df(b1$x3p[[1]])
+#
+# save(b1, b1_l1, file = "tests/testdata/crosscut_test_data.Rdata")
 
-cc <- land_cc(50, b1_l1)
+load("../testdata/crosscut_test_data.Rdata")
+cc <- bulletxtrctr:::land_cc(50, b1_l1)
 
 test_that("land_cc works as expected", {
   expect_s3_class(cc, "data.frame")
@@ -27,3 +47,4 @@ test_that("x3p_crosscut works as expected", {
   expect_length(attr(cc, "header.info"), 4)
   expect_equal(sort(names(attr(cc, "header.info"))), sort(c("sizeY", "sizeX", "incrementY", "incrementX")))
 })
+
--- tests/testthat/test_grooves.R
+++ tests/testthat/test_grooves.R
@@ -3,9 +3,11 @@ context("grooves")
 library(x3ptools)
 library(bulletxtrctr)
 library(tidyverse)
+#
+# b1 <- read_bullet("../testdata/Bullet1", "x3p")
+# b1_l1 <- x3ptools::x3p_to_df(b1$x3p[[1]])
 
-b1 <- read_bullet("../testdata/Bullet1", "x3p")
-b1_l1 <- x3ptools::x3p_to_df(b1$x3p[[1]])
+load("../testdata/crosscut_test_data.Rdata")
 
 test_that("grooves works as expected", {
   expect_message(res <- cc_locate_grooves(b1_l1), "summarizing \\d{1,} profiles by averaging across values")
