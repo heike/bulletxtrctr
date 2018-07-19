a9679d5e2f52932d71176031e91150e55d368eb4
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-07-19 13:44:52 -0500

    test attempt that isn't working

--- tests/testthat/test_align.R
+++ tests/testthat/test_align.R
@@ -1,6 +1,66 @@
 context("align")
 
 
+library(x3ptools)
+library(bulletxtrctr)
+library(tidyverse)
+
+b1 <- read_bullet("../../data/Bullet1", "x3p") %>%
+# turn the scans such that (0,0) is bottom left
+  mutate(
+    x3p = x3p %>% purrr::map(.f = function(x) x %>%
+                               rotate_x3p(angle=-90) %>%
+                               y_flip_x3p())
+  ) %>% mutate(
+    x3p = x3p %>% purrr::map(.f = function(x) {
+      # make sure all measurements are in microns
+      x$surface.matrix <- x$surface.matrix*10^6
+      x$header.info$incrementY <- x$header.info$incrementY*10^6
+      x$header.info$incrementX <- x$header.info$incrementX*10^6
+      x
+    })
+  ) %>%
+  # filter(row_number() == 1) %>%
+  mutate(crosscut = x3p %>% purrr::map_dbl(.f = x3p_crosscut_optimize)) %>%
+  mutate(ccdata = purrr::map2(.x = x3p, .y = crosscut, .f = x3p_crosscut)) %>%
+  mutate(grooves = purrr::map(ccdata, cc_locate_grooves, method = "middle")) %>%
+  mutate(
+    sigs = purrr::map2(
+      .x = ccdata, .y = grooves,
+      .f = function(x, y) {
+        cc_get_signature(ccdata=x, grooves = y, span1 = 0.75, span2=0.03)})
+  )
+
+b2 <- read_bullet("../../data/Bullet2", "x3p") %>%
+  # turn the scans such that (0,0) is bottom left
+  mutate(
+    x3p = x3p %>% purrr::map(.f = function(x) x %>%
+                               rotate_x3p(angle=-90) %>%
+                               y_flip_x3p())
+  ) %>% mutate(
+    x3p = x3p %>% purrr::map(.f = function(x) {
+      # make sure all measurements are in microns
+      x$surface.matrix <- x$surface.matrix*10^6
+      x$header.info$incrementY <- x$header.info$incrementY*10^6
+      x$header.info$incrementX <- x$header.info$incrementX*10^6
+      x
+    })
+  ) %>%
+  # filter(row_number() == 1) %>%
+  mutate(crosscut = x3p %>% purrr::map_dbl(.f = x3p_crosscut_optimize)) %>%
+  mutate(ccdata = purrr::map2(.x = x3p, .y = crosscut, .f = x3p_crosscut)) %>%
+  mutate(grooves = purrr::map(ccdata, cc_locate_grooves, method = "middle")) %>%
+  mutate(
+    sigs = purrr::map2(
+      .x = ccdata, .y = grooves,
+      .f = function(x, y) {
+        cc_get_signature(ccdata=x, grooves = y, span1 = 0.75, span2=0.03)})
+  )
+
+save(b1, b2, file = "../../tests/testdata/correct_data_test_align.Rdata")
+
+bulletxtrctr::sig_align(b1$sigs[[3]], b2$sigs[[5]])
+
 test_that("align works as expected", {
 
 
