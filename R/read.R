11789ef1bcb597a03a45494ef36d3032f45911ed
Susan Vanderplas <srvanderplas@gmail.com>
2018-08-09 21:36:27 -0500

    url-friendly read_bullet, dataset with hamby252 urls, and remove unnecessary helper.R

--- R/read.R
+++ R/read.R
@@ -3,6 +3,7 @@
 #' some more description
 #' @param folder character describing the path to a folder
 #' @param ext character value, consisting of the extension(s) describing the file format the scans are in
+#' @param urllist list of URLs pointing to x3p files
 #' @return data frame with two variables, source and x3p, containing the path to the file and the corresponding x3p file
 #' @export
 #' @importFrom x3ptools read_x3p
@@ -10,9 +11,18 @@
 #' @examples
 #'  b1 <- read_bullet("data/Bullet1", "x3p")
 #'  b2 <- read_bullet("data/Bullet2", "x3p")
-read_bullet <- function(folder, ext="x3p") {
-  set = dir(folder, pattern=ext, recursive = TRUE, full.names = TRUE)
-  message(sprintf("%d files found. Reading ...", length(set)))
+read_bullet <- function(folder = NULL, ext = "x3p", urllist = NULL) {
+  stopifnot(!is.null(folder) | !is.null(urllist))
+  if (!is.null(folder) & !is.null(urllist)) {
+    message("folder and urllist both provided. Defaulting to reading x3p files from folder.")
+  }
+
+  if (!is.null(folder)) {
+    set <- dir(folder, pattern = ext, recursive = TRUE, full.names = TRUE)
+    message(sprintf("%d files found. Reading ...", length(set)))
+  } else {
+    set <- urllist
+  }
 
   if (ext == "x3p") {
     scans <- lapply(set, FUN = read_x3p)
--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -35,11 +35,33 @@ b3 <- b2 %>%
     })
   )
 
+b4 <- read_bullet(urllist = hamby252demo$bullet1[3]) %>%
+  # turn the scans such that (0,0) is bottom left
+  dplyr::mutate(
+    x3p = x3p %>% purrr::map(.f = function(x) x %>%
+                               x3ptools::rotate_x3p(angle=-90) %>%
+                               x3ptools::y_flip_x3p())
+  ) %>% dplyr::mutate(
+    x3p = x3p %>% purrr::map(.f = function(x) {
+      # make sure all measurements are in microns
+      x$surface.matrix <- x$surface.matrix*10^6
+      x$header.info$incrementY <- x$header.info$incrementY*10^6
+      x$header.info$incrementX <- x$header.info$incrementX*10^6
+      x
+    })
+  )
+
 test_that("read works as expected", {
   expect_length(o1, 0)
   expect_s3_class(b2, "tbl_df")
   expect_s3_class(b2, "tbl")
   expect_s3_class(b2, "data.frame")
   expect_equal(b1_l3_x3p$x3p, b3$x3p)
+  expect_equal(b1_l3_x3p$x3p, b4$x3p)
+
+  # If conditions
+  expect_message(read_bullet(folder = here::here("tests/Bullet1/Hamby252_Barrel1_Bullet1_Land3.x3p"),
+                             urllist = hamby252demo$bullet1[3]), "folder and urllist both provided")
+  expect_error(read_bullet())
 })
 
